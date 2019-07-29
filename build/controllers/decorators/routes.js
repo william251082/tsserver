a6459d500f73f5e2145330eb2bc50844c567189d
william <william.del.rosario@iceshop.nl>
2019-07-29 16:55:32 +0200

    adding controller methods

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -4,6 +4,14 @@ require("reflect-metadata");
 function get(path) {
     return function (target, key, desc) {
         Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'get', target, key);
     };
 }
 exports.get = get;
+function post(path) {
+    return function (target, key, desc) {
+        Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'post', target, key);
+    };
+}
+exports.post = post;
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -3,5 +3,13 @@ import 'reflect-metadata';
 export function get(path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
         Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'get', target, key);
+    };
+}
+
+export function post(path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+        Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'post', target, key);
     };
 }
