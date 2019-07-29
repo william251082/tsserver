6b4b9bc5d44e02c8e6c66df976a97934e00b62ba
william <william.del.rosario@iceshop.nl>
2019-07-29 22:27:26 +0200

    testing use

---
+++ build/controllers/decorators/use.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+var MetadataKeys_1 = require("./MetadataKeys");
+function use(middleware) {
+    return function (target, key, desc) {
+        var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target, key) || [];
+        middlewares.push(middleware);
+        Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.middleware, middlewares.concat([middleware]), target, key);
+    };
+}
+exports.use = use;
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -12,7 +12,7 @@ export function controller(routePrefix: string) {
             const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
             const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
 
-            const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
+            const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
 
             if (path) {
                 router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
