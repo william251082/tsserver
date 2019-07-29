ed64b52184f9c75d40e91a61ec384cee9eac78cb
william <william.del.rosario@iceshop.nl>
2019-07-29 08:51:16 +0200

    parsing form bodies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import {router} from "./routes/loginRoutes";
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(router);
 
 app.listen(3000, () => {
-    console.log('Listening on port 300');
+    console.log('Listening on port 3000');
 });
