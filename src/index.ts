737556429511db16858e10602a9ed080f088b1e5
william <william.del.rosario@iceshop.nl>
2019-07-29 08:39:54 +0200

    using an express router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
+import {router} from "./routes/loginRoutes";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-    <div>
-        <h1>Hi There!</h1>
-    </div>
-    `)
-});
+app.use(router);
 
 app.listen(3000, () => {
     console.log('Listening on port 300');
